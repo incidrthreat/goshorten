dd0df02203bd5796cddb1238c298d32c1a6aee04
incidrthreat <incidrthreat@gmail.com>
2020-06-14 16:35:24 -0500

    no significant change

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	_ proto "github.com/incidrthreat/goshorten/proto/url_service"
+	_ "github.com/incidrthreat/goshorten/protos/url_service"
 )
 
 func main() {
-	proto
+	//proto
 }
