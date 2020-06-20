1ec8cebe9410d1fbde1b3a4fc8894ed306f134ee
incidrthreat <incidrthreat@gmail.com>
2020-06-20 02:29:38 -0500

    built out CreateURL and GetURL functions

--- backend/server/shortener/shortener.go
+++ backend/server/shortener/shortener.go
@@ -2,30 +2,60 @@ package shortener
 
 import (
 	"context"
-	"fmt"
 
-	pb "github.com/incidrthreat/goshorten/backend/server/pb"
+	pb "github.com/incidrthreat/goshorten/backend/server/protos"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/incidrthreat/goshorten/backend/server/data"
 )
 
-type Server struct{}
+type CreateServer struct {
+	Store data.Redis
+}
 
 var log = hclog.Default()
 
-func (*Server) CreateURL(ctx context.Context, req *pb.ShortURLReq) (*pb.ShortURLResp, error) {
-	if req.LongUrl == "" {
-		log.Error("No URL Reqested")
-	} else {
-		fmt.Printf("URL: %s", req.LongUrl)
-		code := data.GenCode(6)
+// CreateURL ...
+func (c *CreateServer) CreateURL(ctx context.Context, req *pb.ShortURLReq) (*pb.ShortURLResp, error) {
+	url := req.GetLongUrl()
 
-		resp := &pb.ShortURLResp{
-			ShortUrl: "bit.ly/" + code,
-		}
-		return resp, nil
+	if url == "" {
+		log.Error("No URL Reqested")
+		return &pb.ShortURLResp{}, nil
 	}
 
-	return &pb.ShortURLResp{}, nil
+	log.Info("CreateURL Req", "Shorten Request", hclog.Fmt("%s", url))
+
+	code, err := c.Store.Save(url)
+	if err != nil {
+		log.Error("Redis Save:", "Unable to save", err)
+	}
+
+	resp := &pb.ShortURLResp{
+		ShortUrl: "bit.ly/" + code,
+	}
+	return resp, nil
+
+}
+
+// GetURL ...
+func (c *CreateServer) GetURL(ctx context.Context, req *pb.URLReq) (*pb.URLResp, error) {
+	code := req.GetUrlCode()
+	if code == "" {
+		log.Error("GetURL", "Error", "No Code Reqested")
+		return &pb.URLResp{}, nil
+	}
+
+	fullURL, err := c.Store.Load(code)
+	if err != nil {
+		log.Error("Redis Load:", "Unable to Load URL", err)
+		return &pb.URLResp{}, nil
+	}
+
+	resp := &pb.URLResp{
+		RedirectUrl: fullURL,
+	}
+
+	return resp, nil
+
 }
