1109ab50995b85acbc4ab4342680e46e6a6d2353
incidrthreat <incidrthreat@gmail.com>
2020-06-27 03:45:53 -0500

    added url code detection, redirection, and regex

--- frontend/app.js
+++ frontend/app.js
@@ -3,16 +3,32 @@ import Navigo from 'navigo'
 import { Home } from './views/home'
 import { GetUrl } from './views/geturl'
 
-import { ShortenerClient, ShortURLReq } from './pb/url_service_grpc_web_pb'
+import { ShortenerClient, URLReq } from './pb/url_service_grpc_web_pb'
 
-const shortClient = new ShortenerClient('http://grpcbackend:8080', null, null)
+const shortClient = new ShortenerClient('http://localhost:8080')
 
-const router = new Navigo()
+const navRoot = window.location.origin + '/'
+
+const router = new Navigo(navRoot, false)
 
 
 router
-    .on("/", Home)
-    .on("/:code", GetUrl)
+    .on('/', Home)
+    .on(/\/([a-zA-Z0-9]{3,6})\/?/, function (code) {
+        console.log(code)
+        let req = new URLReq()
+        req.setUrlcode(code)
+        shortClient.getURL(req, {}, (err, res) =>{
+            if (err) {
+                alert(err.message)
+                return
+            }
+            if (res.getRedirecturl() == "") {
+                return Home()
+            } 
+            return window.location.replace(res.getRedirecturl())
+        })
+    })
     .resolve()
 
 export { router, shortClient }
