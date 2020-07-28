39936ebd6ae583ea7e546a60f4f464614e5cf067
incidrthreat <incidrthreat@gmail.com>
2020-07-28 15:16:53 -0500

    add ttl and switch/case

--- backend/shortener/shortener.go
+++ backend/shortener/shortener.go
@@ -18,45 +18,60 @@ type CreateServer struct {
 var log = hclog.Default()
 
 // CreateURL ...
-func (c *CreateServer) CreateURL(ctx context.Context, req *pb.ShortURLReq) (*pb.ShortURLResp, error) {
+func (c *CreateServer) CreateURL(ctx context.Context, req *pb.URL) (*pb.Code, error) {
 	url := req.GetLongUrl()
+	ttl := req.GetTTL()
 
-	if url == "" {
-		log.Error("No URL Reqested")
-		return &pb.ShortURLResp{}, errors.New("No URL Requested")
+	// TODO: Fix conditions to accept 5 min, 1 day, or 2 days
+	// if (ttl != "300"; ttl != "86400"; ttl != "172800") {
+	// 	log.Error("TTL Error:", "Error", hclog.Fmt("TTL requested not acceptable"))
+	// 	return &pb.Code{}, errors.New("TTL requested not acceptable")
+	// }
+
+	switch ttl {
+	case "300", "86400", "172800":
+		if url == "" {
+			log.Error("Redis Error:", "Error", hclog.Fmt("No URL Reqested"))
+			return &pb.Code{}, errors.New("No URL Requested")
+		}
+
+		log.Info("CreateURL Req", "Shorten Request", hclog.Fmt("%s", url))
+
+		code, err := c.Store.Save(url, ttl)
+		if err != nil {
+			log.Error("Redis Save:", "Unable to save", err)
+			return &pb.Code{}, errors.New("Unable to store URL")
+		}
+
+		resp := &pb.Code{
+			Code: code,
+		}
+		return resp, nil
+	default:
+		log.Error("TTL Error:", "Error", hclog.Fmt("TTL requested not acceptable"))
+		return &pb.Code{}, errors.New("TTL requested not acceptable")
 	}
-
-	log.Info("CreateURL Req", "Shorten Request", hclog.Fmt("%s", url))
-
-	code, err := c.Store.Save(url)
-	if err != nil {
-		log.Error("Redis Save:", "Unable to save", err)
-		return &pb.ShortURLResp{}, errors.New("Unable to store URL")
-	}
-
-	resp := &pb.ShortURLResp{
-		ShortUrl: code,
-	}
-	return resp, nil
 
 }
 
 // GetURL ...
-func (c *CreateServer) GetURL(ctx context.Context, req *pb.URLReq) (*pb.URLResp, error) {
-	code := req.GetUrlCode()
+func (c *CreateServer) GetURL(ctx context.Context, req *pb.Code) (*pb.URL, error) {
+	code := req.GetCode()
+
 	if code == "" {
 		log.Error("GetURL", "Error", "No Code Reqested")
-		return &pb.URLResp{}, errors.New("No Code Requested")
+		return &pb.URL{}, errors.New("No Code Requested")
 	}
 
 	fullURL, err := c.Store.Load(code)
 	if err != nil {
 		log.Error("Redis Load:", "Unable to Load URL", err)
-		return &pb.URLResp{}, errors.New("URL expired or not in storage")
+		return &pb.URL{}, errors.New("URL expired or not in storage")
 	}
 
-	resp := &pb.URLResp{
-		RedirectUrl: fullURL,
+	resp := &pb.URL{
+		LongUrl: fullURL,
+		TTL:     "",
 	}
 
 	return resp, nil
