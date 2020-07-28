8f2c839655a7e386a362ee3dc6ed1635dccc0370
incidrthreat <incidrthreat@gmail.com>
2020-07-28 15:17:13 -0500

    added TLS connection to collision test

--- collision_tests/main.go
+++ collision_tests/main.go
@@ -8,11 +8,17 @@ import (
 
 	pb "github.com/incidrthreat/goshorten/frontend-go/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial("localhost:9000", grpc.WithInsecure())
+	clientCert, err := credentials.NewClientTLSFromFile("server.crt", "")
+	if err != nil {
+		log.Fatal("Couldnt find the file.")
+	}
+
+	conn, err = grpc.DialContext(context.Background(), "localhost:9000", grpc.WithTransportCredentials(clientCert))
 	if err != nil {
 		log.Println(err)
 	}
@@ -23,8 +29,8 @@ func main() {
 	start := time.Now()
 	i := 1
 
-	for i <= 99999 {
-		resp, err := c.CreateURL(context.Background(), &pb.ShortURLReq{LongUrl: "test" + strconv.Itoa(i)})
+	for i <= 9999 {
+		resp, err := c.CreateURL(context.Background(), &pb.URL{LongUrl: "test" + strconv.Itoa(i)})
 		if err != nil {
 			log.Println(err)
 		}
