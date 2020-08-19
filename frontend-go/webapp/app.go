ce18a14d30975445e3edd9095b93c1504906c625
incidrthreat <incidrthreat@gmail.com>
2020-08-19 15:11:57 -0500

    added HTMLData and Statistics struct

--- frontend-go/webapp/app.go
+++ frontend-go/webapp/app.go
@@ -1,10 +1,28 @@
 package webapp
 
-import "google.golang.org/grpc"
+import (
+	"google.golang.org/grpc"
+)
 
-// App ...
+// App struct
 type App struct {
 	HTMLDir   string
 	StaticDir string
 	Conn      *grpc.ClientConn
+}
+
+// HTMLData struct for passing data to html templates
+type HTMLData struct {
+	Data  string
+	Error string
+	Stats Statistics
+}
+
+// Statistics defines the Code:URL structure
+type Statistics struct {
+	Code         string `json:"code"`
+	URL          string `json:"url"`
+	CreatedAt    string `json:"created_at"`
+	LastAccessed string `json:"last_accessed"`
+	Clicks       string `json:"clicks"`
 }
